3d0456bc363882b282f1071b82f7e26298e2bd2a
Wil Thieme <wil.thieme@protonmail.com>
2020-06-09 09:16:48 -0400

    v1.2.2
    It's almost working, I think.
    
    Co-Authored-By: Paul Caruso <crusopaul@gmail.com>

--- campaign/scripts/real_encumbrance.lua
+++ campaign/scripts/real_encumbrance.lua
@@ -3,81 +3,163 @@
 --
 
 function onInit()
-	 local node = getDatabaseNode();
-	 DB.addHandler(DB.getPath(node, "*.carried"), "onUpdate", TotalEncumbranceChanged);
+	DB.addHandler( DB.getPath( getDatabaseNode(), 'inventorylist.*.carried' ), 'onUpdate', ApplyPenalties )
 end
 
 function onClose()
-	 local node = getDatabaseNode();
-	 DB.removeHandler(DB.getPath(node, "*.carried"), "onUpdate", TotalEncumbranceChanged);
+	DB.removeHandler( DB.getPath( getDatabaseNode(), 'inventorylist.*.carried' ), 'onUpdate', ApplyPenalties )
 end
 
-function TotalEncumbranceChanged()
-	local nodeWin = window.getDatabaseNode()
-    local rActor = ActorManager.getActor("pc", nodeWin )
-    local nodePC = DB.findNode(rActor['sCreatureNode'])
-	-- Debug.chat('1', nodePC.getParent() )
-		nodePC = ( nodePC.getParent() ).getName() == 'inventorylist' and ( nodePC.getParent() ).getParent() or nodePC
-
-    local total = DB.getValue( nodePC.getPath() .. '.encumbrance.total' )
-    local medium = DB.getValue( nodePC.getPath() .. '.encumbrance.mediumload' )
-    local heavy = DB.getValue( nodePC.getPath() .. '.encumbrance.heavyload' )
-
-    local maxstatfromarmor = DB.getValue( nodePC.getPath() .. '.encumbrance.armormaxstatbonus' )
-    local maxstatfromenc
+--Summary: Recomputes penalties and updates max stat and check penalty
+--Arguments: nodeField - node of 'carried' when called from handler
+function ApplyPenalties(nodeField)
+	local nodePC = HandleApplyPenaltiesArgs(nodeField)
 	local maxstattoset
-	
-	local checkpenalty = DB.getValue( nodePC.getPath() .. '.encumbrance.armorcheckpenalty' )
-	local checkpenaltyfromenc
 	local checkpenaltytoset
-	
-	-- Debug.chat('2',nodePC.getPath())
-	
-	--define penalties
-	if total >= heavy then
-        maxstatfromenc = 1
-        checkpenaltyfromenc = -6
-    elseif total >= medium then
-        maxstatfromenc = 3
-        checkpenaltyfromenc = -3
-    else
-        maxstatfromenc = 0
-		checkpenaltyfromenc = 0
-    end
-	
-	--eliminate potential zero results from max stat
-	if (maxstatfromarmor == nil or maxstatfromarmor == 0) and  (maxstatfromenc == nil or maxstatfromenc == 0) then
-		maxstattoset = 0
-	elseif maxstatfromarmor == nil or maxstatfromarmor == 0 then
-		maxstattoset = maxstatfromenc
-	elseif maxstatfromenc == nil or maxstatfromenc == 0 then
-		maxstattoset = maxstatfromarmor
-	else
-		maxstattoset = math.min(maxstatfromarmor, maxstatfromenc)
-	end
-	
-	--eliminate potential zero results from check penalty
-	if (checkpenalty == nil or checkpenalty == 0) and  (checkpenaltyfromenc == nil or checkpenaltyfromenc == 0) then
-		checkpenaltytoset = checkpenaltyfromenc
-	elseif checkpenaltyfromenc == nil or checkpenaltyfromenc == 0 then
-		checkpenaltytoset = checkpenalty
-	else
-		checkpenaltytoset = math.min(checkpenalty, checkpenaltyfromenc)
-	end
+	local spellfailuretoset
+	maxstattoset, checkpenaltytoset, spellfailuretoset = ComputePenalties(nodePC)
 
-	DB.setValue( nodePC.getPath() .. '.encumbrance.armormaxstatbonusfromenc', 'number', maxstatfromenc )
-	DB.setValue( nodePC.getPath() .. '.encumbrance.checkpenaltyfromenc', 'number', checkpenaltyfromenc )
-	DB.setValue( nodePC.getPath() .. '.encumbrance.armormaxstatbonus', 'number', maxstattoset )
-    DB.setValue( nodePC.getPath() .. '.encumbrance.armorcheckpenalty', 'number', checkpenaltytoset )
+	DB.setValue( nodePC, 'encumbrance.armormaxstatbonus', 'number', maxstattoset )
+	DB.setValue( nodePC, 'encumbrance.armorcheckpenalty', 'number', checkpenaltytoset )
 	
-    DB.setValue( nodePC.getPath() .. '.encumbrance.armormaxstatbonusactive', 'number', 1 )
-	-- Debug.chat('checkpenalty',checkpenaltytoset,checkpenalty,checkpenaltyfromenc)
-	-- Debug.chat('maxstatfromarmor',maxstattoset,maxstatfromarmor,maxstatfromenc)
-
+	--enable armor encumbrance when needed
+	if maxstattoset ~= 0 or checkpenaltytoset ~= 0 or spellfailuretoset ~= 0 then
+		DB.setValue( nodePC, 'encumbrance.armormaxstatbonusactive', 'number', 1 )
+	else
+		DB.setValue( nodePC, 'encumbrance.armormaxstatbonusactive', 'number', 0 )
+	end
 end
 
--- This function queries the total encumbrance
-function onValueChanged()
+--Summary: Handles arguments of ApplyPenalties()
+--Argument: potentially nil nodeField representing carried databasenode on newly carried / equipped / dropped item
+--Return: appropriate object databasenode - should represent node of PC
+function HandleApplyPenaltiesArgs(nodeField)
+	local nodePC
+	
+	if nodeField ~= nil then
+		nodePC = nodeField.getChild( '....' )
+	else
+		local nodeWin = window.getDatabaseNode()
+		local rActor = ActorManager.getActor( 'pc', nodeWin )
+		nodePC = DB.findNode( rActor['sCreatureNode'] )
+	end
+	return nodePC
+end
 
-    TotalEncumbranceChanged()
+--Summary: Finds the max stat and check penalty penalties based on medium and heavy encumbrance thresholds based on current total encumbrance
+--Argument: number light is medium encumbrance threshold for PC
+--Argument: number light is heavy encumbrance threshold for PC
+--Argument: number total is current total encumbrance for PC
+--Return: number for max stat penalty based solely on encumbrance (max stat, skill penalty, spell failure chance)
+--Return: number for check penalty penalty based solely on encumbrance (max stat, skill penalty, spell failure chance)
+--Return: number for spell failure chance based solely on encumbrance (max stat, skill penalty, spell failure chance)
+function EncumbrancePenalties(light, medium, total)
+	if total > medium then -- heavy load
+		return 1, -6, 0
+	elseif total > light then -- medium load
+		return 3, -3, 0
+	else -- light load
+		return 0, 0, 0
+	end
+end
+
+--Summary: Finds max stat / check penalty tables with appropriate nonzero values
+--Argument: databasenode nodePC is the PC node
+--Return: table holding nonzero max stat penalties from armor / shields
+--Return: table holding nonzero check penalty penalties from armor / shields
+function RawArmorPenalties(nodePC)
+	local maxstattable = {}
+	local checkpenaltytable	= {}
+	local spellfailuretable = {}
+
+	--Populates penalty tables from armor / shields
+	local maxstatcount = 1
+	local checkpenaltycount = 1
+	local spellfailurecount = 1
+	for _,v in pairs( DB.getChildren( nodePC, 'inventorylist' ) ) do
+		local itemcarried = DB.getValue( v, 'carried', 0 )
+		local itemmaxstat = DB.getValue( v, 'maxstatbonus', 0 )
+		local itemcheckpenalty = DB.getValue( v, 'checkpenalty', 0)
+		local itemspellfailure = DB.getValue( v, 'spellfailure', 0)
+
+		if itemcarried == 2 then
+			if itemmaxstat ~= 0 then
+				maxstattable[maxstatcount] = itemmaxstat
+				maxstatcount = maxstatcount + 1
+			end
+			if itemcheckpenalty ~= 0 then
+				checkpenaltytable[checkpenaltycount] = itemcheckpenalty
+				checkpenaltycount = checkpenaltycount + 1
+			end
+			if itemspellfailure ~= 0 then
+				spellfailuretable[spellfailurecount] = itemspellfailure
+				spellfailurecount = spellfailurecount + 1
+			end
+		end
+	end
+
+	return maxstattable, checkpenaltytable, spellfailuretable
+end
+
+--Summary: Appends encumbrance-based penalties to respective penalty tables
+--Argument: databasenode nodePC is the PC node
+--Argument: table holding nonzero max stat penalties from armor / shields
+--Argument: table holding nonzero check penalty penalties from armor / shields
+function RawEncumbrancePenalties(nodePC, maxstattable, checkpenaltytable, spellfailurefromenc)
+	local light = DB.getValue( nodePC, 'encumbrance.lightload', 0 )
+	local medium = DB.getValue( nodePC, 'encumbrance.mediumload', 0 )
+	local total = DB.getValue( nodePC, 'encumbrance.total', 0 )
+	
+	local maxstatfromenc
+	local checkpenaltyfromenc
+	local spellfailurefromenc
+	local maxstatfromenc, checkpenaltyfromenc, spellfailuretable = EncumbrancePenalties(light, medium, total)
+	DB.setValue( nodePC, 'encumbrance.armormaxstatbonusfromenc', 'number', maxstatfromenc )
+	DB.setValue( nodePC, 'encumbrance.checkpenaltyfromenc', 'number', checkpenaltyfromenc )
+	
+	local tblcount = table.getn( maxstattable )
+	if maxstatfromenc ~= 0 then
+		maxstattable[tblcount + 1] = maxstatfromenc
+	end
+	
+	tblcount = table.getn( checkpenaltytable )
+	if checkpenaltyfromenc ~= 0 then
+		checkpenaltytable[tblcount + 1] = checkpenaltyfromenc
+	end
+end
+
+--Summary: Finds max stat and check penalty based on current enc / armor / shield data
+--Argument: databasenode nodePC is the PC node
+--Return: number holding armor max stat penalty
+--Return: number holding armor check penalty
+function ComputePenalties(nodePC)
+	local maxstattable = {}
+	local checkpenaltytable = {}
+	local spellfailuretable = {}
+	maxstattable, checkpenaltytable, spellfailuretable = RawArmorPenalties(nodePC)
+	RawEncumbrancePenalties(nodePC, maxstattable, checkpenaltytable, spellfailuretable)
+	
+	local maxstattoset
+	local checkpenaltytoset
+	local spellfailuretoset
+
+	if table.getn( maxstattable ) ~= 0 then
+		maxstattoset = math.min( unpack( maxstattable ) )
+	else
+		maxstattoset = 0
+	end
+	
+	if table.getn( checkpenaltytable ) ~= 0 then
+		checkpenaltytoset = math.min( unpack( checkpenaltytable ) )
+	else
+		checkpenaltytoset = 0
+	end
+	
+	if table.getn( spellfailuretable ) ~= 0 then
+		spellfailuretoset = math.max( unpack( spellfailuretable ) )
+	else
+		spellfailuretoset = 0
+	end
+	
+	return maxstattoset, checkpenaltytoset, spellfailuretoset
 end
