ec16e8f7437638a28b9a25ec79381e1f2f43e706
Wil Thieme <wil.thieme@protonmail.com>
2020-06-15 15:54:27 -0400

    Update spell_failure.lua
    modify logic so that classes that don't have failure on certain categories of armor/shields don't get the chat message about spell failure when wearing that type of shields!

--- scripts/spell_failure.lua
+++ scripts/spell_failure.lua
@@ -9,14 +9,12 @@ function arcaneSpellFailure(nodeSpell)
 
 	if spellfailurechance ~= 0 then
 		-- if true, roll failure chance
-		local arcanemagic
-		arcanemagic = isArcaneCaster(nodeSpellset)
+		local arcanecaster = isArcaneCaster(nodeSpellset)
 
 		-- if true, don't roll failure chance
-		local stillspell
-		stillspell = isSomaticSpell(nodeSpell)
+		local stillspell = isSomaticSpell(nodeSpell)
 
-		if arcanemagic == true and stillspell == false then
+		if arcanecaster == true and stillspell == false then
 			ChatManager.SystemMessage('Arcane Spell Failure: '..spellfailurechance..'%')
 		end
 
@@ -28,19 +26,51 @@ end
 --Determine if the spell is from a spellset that is on the arcane casters list
 function isArcaneCaster(nodeSpellset)
 	-- classes that should roll arcane failure if encumbered by armor/shields
-	local arcaneclasses = {'Bard', 'Sorcerer', 'Wizard', 'Magus', 'Summoner', 'Witch', 'Arcanist', 'Bloodrager', 'Skald', 'Unchained Summoner'}
-
+	local arcaneclass_heavyarmor = {'Bard', 'Sorcerer', 'Wizard', 'Magus', 'Summoner', 'Witch', 'Arcanist', 'Bloodrager', 'Skald', 'Unchained Summoner'}
+	local arcaneclass_medarmor = {'Bard', 'Sorcerer', 'Wizard', 'Magus', 'Summoner', 'Witch', 'Arcanist', 'Unchained Summoner'}
+	local arcaneclass_ltarmor = {'Sorcerer', 'Wizard', 'Witch', 'Arcanist'}
+	local arcaneclass_shield = {'Sorcerer', 'Wizard', 'Magus', 'Summoner', 'Witch', 'Arcanist', 'Bloodrager', 'Unchained Summoner'}
 	local playerspellset = DB.getValue(nodeSpellset, 'label')
-	
-	arcanemagic = false
 
-	for _,v in pairs(arcaneclasses) do
-		if v == playerspellset then
-			arcanemagic = true
+	local armorcategory = DB.getValue(nodeSpellset.getChild('...'), 'encumbrance.armorcategory')
+	local shieldequipped = DB.getValue(nodeSpellset.getChild('...'), 'encumbrance.shieldequipped')
+
+	arcanecaster = false
+
+	if armorcategory == 3 then --if PC has is wearing heavy armor
+		for _,v in pairs(arcaneclass_heavyarmor) do
+			if v == playerspellset then
+				arcanecaster = true
+			end
+		end
+	elseif armorcategory == 2 then --if PC has is wearing medium armor
+		for _,v in pairs(arcaneclass_medarmor) do
+			if v == playerspellset then
+				arcanecaster = true
+			end
+		end
+	elseif armorcategory == 1 then --if PC has is wearing light armor
+		for _,v in pairs(arcaneclass_ltarmor) do
+			if v == playerspellset then
+				arcanecaster = true
+			end
 		end
 	end
-	
-	return arcanemagic
+	if shieldequipped == 2 then -- if PC has a tower shield equipped
+		for _,v in pairs(arcaneclass_heavyarmor) do
+			if v == playerspellset then
+				arcanecaster = true
+			end
+		end
+	elseif shieldequipped == 1 then -- if PC has a shield equipped
+		for _,v in pairs(arcaneclass_shield) do
+			if v == playerspellset then
+				arcanecaster = true
+			end
+		end
+	end
+
+	return arcanecaster
 end
 
 -- Convert from CSV string to table (converts a single line of a CSV file)
