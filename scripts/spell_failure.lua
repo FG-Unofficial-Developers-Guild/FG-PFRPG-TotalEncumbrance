398d9720750a0161388b844ee50233e5f383c4c8
Wil Thieme <wil.thieme@protonmail.com>
2020-06-15 12:41:29 -0400

    Triggering for casting with arcane class
    This doesn't trigger anything but a debug message, but it's doing the right stuff. Right now it doesn't work off the 'combo' cast button, but that can be addressed later.

--- scripts/spell_failure.lua
+++ scripts/spell_failure.lua
@@ -8,3 +8,23 @@ end
 function getRoll()
 
 end
+
+function arcaneSpellFailure(nodeSpellset)
+
+	arcanemagic = isArcaneCaster(nodeSpellset)
+end
+
+function isArcaneCaster(nodeSpellset)
+	local playerspellset = DB.getValue(nodeSpellset, 'label')
+
+	-- classes that should roll arcane failure if encumbered by armor/shields
+	local arcaneclasses = {'Bard', 'Sorcerer', 'Wizard'}
+
+	for _,v in pairs(arcaneclasses) do
+		if v == playerspellset then
+		-- roll percentile dice for arcane failure and parse result based on encumbrance.spellfailure
+		-- output result to chat
+		Debug.chat('arcane spellset '..playerspellset..' detected')
+		end
+	end
+end
