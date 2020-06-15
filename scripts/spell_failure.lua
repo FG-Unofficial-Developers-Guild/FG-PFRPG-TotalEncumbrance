7763b6418bf2fef13547dd1a90667b1667c809b1
Wil Thieme <wil.thieme@protonmail.com>
2020-06-15 13:20:09 -0400

    begin implementation of components check
    spells that don't require physical movements don't have a failure chance.
    when the cast button is pressed on one of these spells, it should ideally not roll the failure chance

--- scripts/spell_failure.lua
+++ scripts/spell_failure.lua
@@ -1,30 +1,51 @@
 -- 
 -- Please see the license.html file included with this distribution for attribution and copyright information.
 --
-function onInit()
-	
-end
 
-function getRoll()
+--Automatically determine if arcane failure chance should be rolled when a spell's cast button is clicked
+function arcaneSpellFailure(nodeSpell)
+	local nodeSpellset = nodeSpell.getChild('.....')
 
-end
-
-function arcaneSpellFailure(nodeSpellset)
-
+	-- if true, roll failure chance
+	local arcanemagic
 	arcanemagic = isArcaneCaster(nodeSpellset)
+
+	-- if true, don't roll failure chance
+	local stillspell
+	stillspell = isSomaticSpell(nodeSpell)
+
+	if arcanemagic == true and stillspell == false then
+		Debug.chat('spell failure!')
+	end
+
+	-- roll percentile dice for arcane failure and parse result based on encumbrance.spellfailure
+	-- output result to chat
 end
 
+--Determine if the spell is from a spellset that is on the arcane casters list
 function isArcaneCaster(nodeSpellset)
-	local playerspellset = DB.getValue(nodeSpellset, 'label')
-
 	-- classes that should roll arcane failure if encumbered by armor/shields
 	local arcaneclasses = {'Bard', 'Sorcerer', 'Wizard'}
 
+	local playerspellset = DB.getValue(nodeSpellset, 'label')
+	
+	arcanemagic = false
+
 	for _,v in pairs(arcaneclasses) do
 		if v == playerspellset then
-		-- roll percentile dice for arcane failure and parse result based on encumbrance.spellfailure
-		-- output result to chat
-		Debug.chat('arcane spellset '..playerspellset..' detected')
+			arcanemagic = true
 		end
 	end
+	
+	return arcanemagic
+end
+
+--Determine if the spell requires somatic compenents
+function isSomaticSpell(nodeSpell)
+	local components = DB.getValue(nodeSpell,'components')
+	Debug.chat('components',components)
+
+	stillspell = false
+
+	
 end
