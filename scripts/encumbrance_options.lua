c6ce4ae5458dd837ccc85fd31c6161a93f8038f9
Wil Thieme <wil.thieme@protonmail.com>
2020-06-14 23:15:58 -0400

    shorter strings
    also tidied up and de-dumbed the registerOption2 code

--- scripts/encumbrance_options.lua
+++ scripts/encumbrance_options.lua
@@ -1,31 +1,17 @@
 --[[
     Please see the license.html file included with this distribution for attribution and copyright information.
 --]]
-local oXPType = ""
-
-
 function onInit()
-	OptionsManager.registerOption2("WEIGHT_ENCUMBRANCE", false, "option_header_game", "option_label_weight_encumbrance", "option_entry_cycler", 
-		{ labels = "encumbrance_options_penalties_on|encumbrance_options_penalties_off", 
-		  values = "1|0", 
-		  baselabel = "encumbrance_options_penalties_on", 
-		  baseval = "on", 
-		  default = "1" }
-	);
+	registerOptions()
+end
 
-	OptionsManager.registerOption2("COIN_WEIGHT", false, "option_header_game", "option_label_coin_weight", "option_entry_cycler", 
-		{ labels = "encumbrance_options_coinweight_on|encumbrance_options_coinweight_off", 
-		  values = "1|0", 
-		  baselabel = "encumbrance_options_coinweight_on", 
-		  baseval = "on", 
-		  default = "1" }
-	);	
-	
-	OptionsManager.registerOption2("ENCUMBRANCE_COLORS", false, "option_header_game", "option_label_encumbrance_colors", "option_entry_cycler", 
-		{ labels = "encumbrance_options_color_on|encumbrance_options_color_off", 
-		  values = "1|0", 
-		  baselabel = "encumbrance_options_color_off", 
-		  baseval = "off", 
-		  default = "0" }
-	);	
+function registerOptions()
+	OptionsManager.registerOption2("WEIGHT_ENCUMBRANCE", false, "option_header_game", "opt_lab_weight_enc", "option_entry_cycler", 
+		{ labels = "enc_opt_pen_on|enc_opt_pen_off", values = "on|off", baselabel = "enc_opt_pen_on", baseval = "on", default = "off" })
+
+	OptionsManager.registerOption2("COIN_WEIGHT", false, "option_header_game", "opt_lab_coin_weight", "option_entry_cycler", 
+		{ labels = "enc_opt_coin_on|enc_opt_coin_off", values = "on|off", baselabel = "enc_opt_coin_on", baseval = "on", default = "off" })
+
+	OptionsManager.registerOption2("ENCUMBRANCE_COLORS", false, "option_header_game", "opt_lab_enc_color", "option_entry_cycler", 
+		{ labels = "enc_opt_color_on|enc_opt_color_off", values = "on|off", baselabel = "enc_opt_color_off", baseval = "off", default = "on" })
 end
