2d1c5fba988ac34b9ed375438db76e77add3966e
Wil Thieme <wil.thieme@protonmail.com>
2020-06-15 21:21:28 -0400

    asmr edition
    the sounds of dice rolling are music to my ears

--- scripts/spell_failure.lua
+++ scripts/spell_failure.lua
@@ -1,11 +1,20 @@
 -- 
--- Please see the license.html file included with this distribution for attribution and copyright information.
+--	Please see the license.html file included with this distribution for attribution and copyright information.
 --
 
---Automatically determine if arcane failure chance should be rolled when a spell's cast button is clicked
+function onInit()
+	OOBManager.registerOOBMsgHandler(OOB_MSGTYPE_APPLYSAVE, handleApplySave);
+
+	ActionsManager.registerResultHandler("spellfailure", onRoll);
+end
+
+--	Automatically determine if arcane failure chance should be rolled when a spell's cast button is clicked
 function arcaneSpellFailure(nodeSpell)
 	local nodeSpellset = nodeSpell.getChild('.....')
 	local spellfailurechance = DB.getValue(nodeSpellset.getChild('...'), 'encumbrance.spellfailure')
+
+	local nodeChar = nodeSpellset.getChild("...")
+	local rActor = ActorManager.getActor("pc", nodeChar);
 
 	if spellfailurechance ~= 0 then
 		-- if true, roll failure chance
@@ -14,16 +23,14 @@ function arcaneSpellFailure(nodeSpell)
 		-- if true, don't roll failure chance
 		local stillspell = isSomaticSpell(nodeSpell)
 
-		if arcanecaster == true and stillspell == false then
-			ChatManager.SystemMessage('Arcane Spell Failure: '..spellfailurechance..'%')
-		end
-
 		-- roll percentile dice for arcane failure and parse result based on encumbrance.spellfailure
-		-- output result to chat
+		if arcanecaster == true and stillspell == false then
+			performRoll(draginfo, rActor, spellfailurechance)
+		end
 	end
 end
 
---Determine if the spell is from a spellset that is on the arcane casters list
+--	Determine if the spell is from a spellset that is on the arcane casters list
 function isArcaneCaster(nodeSpellset)
 	-- these are the classes that should roll arcane failure chance when encumbered by these types of armor/shields
 	local arcaneclass_heavyarmor = {'Bard', 'Sorcerer', 'Wizard', 'Magus', 'Summoner', 'Witch', 'Arcanist', 'Bloodrager', 'Skald', 'Unchained Summoner'}
@@ -73,7 +80,7 @@ function isArcaneCaster(nodeSpellset)
 	return arcanecaster
 end
 
--- Convert from CSV string to table (converts a single line of a CSV file)
+--	Convert from CSV string to table (converts a single line of a CSV file)
 function fromCSV(s)
 	s = s .. ','        -- ending comma
 	local t = {}        -- table to collect fields
@@ -87,7 +94,7 @@ function fromCSV(s)
 	return t
 end
 
---Determine if the spell requires somatic compenents
+--	Determine if the spell requires somatic compenents
 function isSomaticSpell(nodeSpell)
 	local components = DB.getValue(nodeSpell,'components')
 	local componentstable = fromCSV(components)
@@ -101,4 +108,42 @@ function isSomaticSpell(nodeSpell)
 	end
 
 	return stillspell
+end
+
+--	roll percentile when in danger of spell failure
+function getRoll()
+	local rRoll = {}
+	rRoll.sType = 'spellfailure'
+	rRoll.aDice = {'d100','d10'}
+	rRoll.sDesc = 'Spell Failure Chance'
+	rRoll.nMod = 0
+
+	return rRoll
+end
+
+function performRoll(draginfo, rActor, nTargetDC)
+	local rRoll = getRoll()
+
+	rRoll.nTarget = nTargetDC
+--	rRoll.bVsSave = true;
+
+	ActionsManager.performAction(draginfo, rActor, rRoll)
+end
+
+function onRoll(rSource, rTarget, rRoll)
+	local rMessage = 'Spell Failure Roll: '
+
+	if rRoll.nTarget then
+		local nTotal = ActionsManager.total(rRoll)
+		local nTargetDC = tonumber(rRoll.nTarget) or 0;
+		
+		rMessage = rMessage .. nTotal .." (vs. DC " .. nTargetDC .. ")"
+		if nTotal >= nTargetDC then
+			rMessage.text = rMessage.text .. " [CAST]"
+		else
+			rMessage.text = rMessage.text .. " [FAILED]"
+		end
+	end
+	
+	Comm.deliverChatMessage(rMessage)
 end
