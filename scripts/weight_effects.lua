5d5d645a27d6dc1c20d06eec51285a3df90742d6
Wil Thieme <wil.thieme@protonmail.com>
2020-06-18 16:23:53 -0400

    hulk smash
    It works great now :)
    I got in way too deep before!

--- scripts/weight_effects.lua
+++ scripts/weight_effects.lua
@@ -11,20 +11,49 @@ end
 --Argument: databasenode nodeField representing effects or label
 --Return: appropriate object databasenode - should represent effects
 local function handleApplyStrengthEffectsArgs(node)
-	local nodeEffects
 
-	Debug.chat(node)
-
-	if node.getChild('...').getName() == 'effects' then
-		nodeEffects = node
-		Debug.chat(nodeEffects)
+	if node.getName() == 'effects' then
+	elseif node.getParent().getName() == 'charsheet' then
+		nodePC = node
 	else
 		Debug.chat('Node error. Unrecognized Node '..node.getPath())
 	end
 
-	return nodeEffects
+	local rActor = ActorManager.getActor('pc', nodePC)
+
+	return nodePC, rActor
 end
 
 function combineCarryModifiers(node)
-	local nodeEffects = handleApplyStrengthEffectsArgs(node)
+	local nodePC, rActor = handleApplyStrengthEffectsArgs(node)
+	local nEffectMod = getEffectsBonus(rActor, 'strength')
+	local nManualStrAdj = DB.getValue(nodePC, 'encumbrance.manualstradj')
+	local nStrAdjToSet = 0
+
+	if nEffectMod then
+		nStrAdjToSet = nStrAdjToSet + nEffectMod
+	end
+	if nManualStrAdj then
+		nStrAdjToSet = nStrAdjToSet + nManualStrAdj
+	end
+
+	DB.setValue(nodePC, 'encumbrance.stradj', 'number', nStrAdjToSet)
+	DB.setValue(nodePC, 'encumbrance.strbonusfromeffects', 'number', nEffectMod)
+end
+
+--	Determine the total bonus to STR from effects
+function getEffectsBonus(rActor, sAbility)
+	if not rActor or not sAbility then
+		return 0, 0
+	end
+
+	local nEffectMod = EffectManager35E.getEffectsBonus(rActor, 'STR', true)
+
+	if EffectManager35E.hasEffectCondition(rActor, "Exhausted") then
+		nEffectMod = nEffectMod - 6
+	elseif EffectManager35E.hasEffectCondition(rActor, "Fatigued") then
+		nEffectMod = nEffectMod - 2
+	end
+
+	return nEffectMod
 end
