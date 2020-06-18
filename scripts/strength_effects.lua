8fffd9388e50d4b775f83f6dac54d5faecfd800f
Wil Thieme <wil.thieme@protonmail.com>
2020-06-18 12:11:50 -0400

    WIP
    I think I'm finally starting to understand this databasenode stuff pretty well!

--- scripts/strength_effects.lua
+++ scripts/strength_effects.lua
@@ -4,7 +4,9 @@
 
 --Summary: you know
 function onInit()
-	DB.addHandler(DB.getPath('combattracker.list.*.effects'), 'onChildUpdate', applyStrengthEffects)
+	DB.addHandler(DB.getPath('combattracker.list.*.effects.*.isactive'), 'onUpdate', applyStrengthEffects)
+	DB.addHandler(DB.getPath('combattracker.list.*.effects.*.label'), 'onUpdate', applyStrengthEffects)
+	DB.addHandler(DB.getPath('combattracker.list.*.effects'), 'onChildDeleted', applyStrengthEffects)
 end
 
 --Summary: Handles arguments of applyStrengthEffects()
@@ -13,59 +15,89 @@ end
 local function nodeConcierge(nodeWin)
 	local nodeEffects
 	local nodePC
-	local playerid
+	local sPlayerID
 
-	if nodeWin.getName() == 'effects' then
+	if nodeWin.getChild('...').getName() == 'effects' then
+		nodeEffects = nodeWin.getChild('...')
+		sPlayerID = nodeEffects.getParent().getName()
+		nodePC = nodeEffects.getChild('.....').getChild('charsheet').getChild(sPlayerID)
+		rActor = ActorManager.getActor('pc', nodePC)
+		rActor.sCTNode = nodeEffects.getChild('..').getPath()
+		rActor.sCreatureNode = nodePC.getPath()
+	elseif nodeWin.getName() == 'effects' then
 		nodeEffects = nodeWin
-		playerid = nodeEffects.getParent().getName()
-		nodePC = nodeWin.getChild('.....').getChild('charsheet').getChild(playerid)
+		sPlayerID = nodeEffects.getParent().getName()
+		nodePC = nodeEffects.getChild('.....').getChild('charsheet').getChild(sPlayerID)
+		rActor = ActorManager.getActor('pc', nodePC)
+		rActor.sCTNode = nodeEffects.getChild('..').getPath()
 	elseif nodeWin.getParent().getName() == 'charsheet' then
 		nodePC = nodeWin
-		playerid = nodeWin.getName()
-		nodeEffects = nodePC.getChild('...').getChild('combattracker').getChild('list').getChild(playerid).getChild('effects')
+		sPlayerID = nodeWin.getName()
+		nodeEffects = nodePC.getChild('...').getChild('combattracker').getChild('list').getChild(sPlayerID).getChild('effects')
+		rActor = ActorManager.getActor('pc', nodePC)
+		rActor.sCTNode = nodeEffects.getChild('..').getPath()
 	else
 		Debug.chat('Node error. Unrecognized Node '..nodeWin.getPath())
 	end
 
-	return nodeEffects, nodePC
+	return nodeEffects, nodePC, rActor
 end
 
 --Summary: Recomputes bonuses from effects and writes them to stradj
 --Argument: databasenode nodeWin representing effects or label
 function applyStrengthEffects(nodeWin)
-	local nodeEffects, nodePC = nodeConcierge(nodeWin)	
-	local rActor = ActorManager.getActor('pc', nodePC)
-	
-	nAbility = ActorManager2.getAbilityEffectsBonus(rActor, 'strength')
-	Debug.chat('effects mod',nAbility)
+	local nodeEffects, nodePC, rActor = nodeConcierge(nodeWin)
 
-	DB.setValue(nodePC, 'encumbrance.strbonusfromeffects', 'number', nAbility)
+	local nEffectMod = getEffectsBonus(rActor, 'strength')
 
-	local totalencstradj = combineSTRCarryModifiers(nodePC)
+	Debug.chat('nEffectMod: ',nEffectMod)
+	Debug.chat(nodePC)
 
-	if totalencstradj == nil then
+	DB.setValue(nodePC, 'encumbrance.strbonusfromeffects', 'number', nEffectMod)
+
+	local nTotalEncStrAdj = combineSTRCarryModifiers(nodePC, nEffectMod)
+
+	if nTotalEncStrAdj == nil then
 		DB.setValue(nodePC, 'encumbrance.stradj', 'number', 0)
 	end
-	if totalencstradj ~= nil then
-		DB.setValue(nodePC, 'encumbrance.stradj', 'number', totalencstradj)
+	if nTotalEncStrAdj ~= nil then
+		DB.setValue(nodePC, 'encumbrance.stradj', 'number', nTotalEncStrAdj)
 	end
 end
 
-function combineSTRCarryModifiers(nodePC)
+--	Determine the total bonus to STR from effects
+function getEffectsBonus(rActor, sAbility)
+	if not rActor or not sAbility then
+		return 0, 0
+	end
+
+	local nEffectMod, nAbilityEffects = EffectManager35E.getEffectsBonus(rActor, 'STR', true)
+
+	if EffectManager35E.hasEffectCondition(rActor, "Exhausted") then
+		nEffectMod = nEffectMod - 6
+		nAbilityEffects = nAbilityEffects + 1
+	elseif EffectManager35E.hasEffectCondition(rActor, "Fatigued") then
+		nEffectMod = nEffectMod - 2
+		nAbilityEffects = nAbilityEffects + 1
+	end
+
+	return nEffectMod
+end
+
+function combineSTRCarryModifiers(nodePC, nEffectMod)
 	local nodeEffects, nodePC = nodeConcierge(nodePC)
 
-	local manualstradj = DB.getValue(nodePC, 'encumbrance.manualstradj')
-	local strbonusfromeffects = DB.getValue(nodePC, 'encumbrance.strbonusfromeffects')
-	local stradjtable = {}
-	
-	if manualstradj ~= nil and manualstradj ~= 0 then
-		table.insert(stradjtable, manualstradj)
+	local nManualStrAdj = DB.getValue(nodePC, 'encumbrance.manualstradj')
+	local tStrAdj = {}
+
+	if nManualStrAdj ~= nil and nManualStrAdj ~= 0 then
+		table.insert(tStrAdj, nManualStrAdj)
 	end
-	if strbonusfromeffects ~= nil and strbonusfromeffects ~= 0 then
-		table.insert(stradjtable, strbonusfromeffects)
+	if nEffectMod ~= nil and nEffectMod ~= 0 then
+		table.insert(tStrAdj, nEffectMod)
 	end
-	
-	local totalencstradj = TotalEncumbranceLib.tableSum(stradjtable)
-	
-	return totalencstradj
+
+	local nTotalEncStrAdj = TotalEncumbranceLib.tableSum(tStrAdj)
+
+	return nTotalEncStrAdj
 end
