6832e0d78595ca2daf35cba9dec59d52aeb40c42
Wil Thieme <wil.thieme@protonmail.com>
2020-06-30 15:27:55 -0400

    improve documentation and remove unused code

--- scripts/coins_weight.lua
+++ scripts/coins_weight.lua
@@ -3,11 +3,11 @@
 --
 
 --	Initialization
-function onInit()
-	if User.isHost() then
-		Comm.registerSlashHandler("ccweight", computeCoinsWeight)
-	end
-end
+-- function onInit()
+	-- if User.isHost() then
+		-- Comm.registerSlashHandler("ccweight", computeCoinsWeight)
+	-- end
+-- end
 
 --	This function recomputes the total weight field
 function recomputeTotalWeight(nodeWin)
@@ -21,20 +21,20 @@ function recomputeTotalWeight(nodeWin)
 end
 
 --	This function is manualy called with the command /ccweight (DM only)
-function computeCoinsWeight(command, parameters)
-	if User.isHost() then
-		for _,v in pairs(DB.getChildren("partysheet.partyinformation")) do
-			local sClass, sRecord = DB.getValue(v, "link")
-			Debug.chat( sRecord );
-			if sClass == "charsheet" and sRecord then
-				local nodePC = DB.findNode(sRecord)
-				if nodePC then
-					computePCCoinsWeigh(nodePC)
-				end
-			end
-		end
-	end
-end
+-- function computeCoinsWeight(command, parameters)
+	-- if User.isHost() then
+		-- for _,v in pairs(DB.getChildren("partysheet.partyinformation")) do
+			-- local sClass, sRecord = DB.getValue(v, "link")
+			-- Debug.chat( sRecord );
+			-- if sClass == "charsheet" and sRecord then
+				-- local nodePC = DB.findNode(sRecord)
+				-- if nodePC then
+					-- computePCCoinsWeigh(nodePC)
+				-- end
+			-- end
+		-- end
+	-- end
+-- end
 
 --	This function is called when a coin field is called
 function onCoinsValueChanged(nodeWin)
--- scripts/lib_total_encumbrance.lua
+++ scripts/lib_total_encumbrance.lua
@@ -2,9 +2,9 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
---	Summary: Sums table values
---	Argument: table t to sum values within
---	Return: sum of values in table t
+---	Calculate the sums of all values in a table
+--	@param t A table containing numbers
+--	@return nSum The sum of all values in table t
 function tableSum(t)
 	local nSum = 0
 
