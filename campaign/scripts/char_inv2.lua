76034f92026feb1565ad89130612cf785c692f58
Wil Thieme <wil.thieme@protonmail.com>
2020-07-13 21:58:56 -0400

    Revert "Ant Hault"
    This reverts commit c0c323ba951774f22017656804d6ee7edb95fba1.

--- campaign/scripts/char_inv2.lua
+++ campaign/scripts/char_inv2.lua
@@ -172,9 +172,5 @@ function getStrEffectBonus(rActor, nStrength)
 		nStrEffectMod = nStrEffectMod + nCarryBonus
 	end
 
-	if EffectManagerTE.hasEffectCondition(rActor, 'Ant Haul') then
-		nStrEffectMod = 3 * nStrEffectMod
-	end
-
 	return nStrEffectMod
 end
