337cc05ee19e71b7a6e686af0a6f72427589013e
Wil Thieme <wil.thieme@protonmail.com>
2020-08-01 14:45:24 -0400

    paring down options

--- scripts/encumbrance_options.lua
+++ scripts/encumbrance_options.lua
@@ -7,27 +7,9 @@ function onInit()
 end
 
 function registerOptions()
-	OptionsManager.registerOption2('WEIGHT_ENCUMBRANCE', false, 'option_header_game', 'opt_lab_weight_enc', 'option_entry_cycler', 
-		{ labels = 'enc_opt_pen_off', values = 'off', baselabel = 'enc_opt_pen_on', baseval = 'on', default = 'on' })
-
-	OptionsManager.registerOption2('COIN_WEIGHT', false, 'option_header_game', 'opt_lab_coin_weight', 'option_entry_cycler', 
-		{ labels = 'enc_opt_coin_off', values = 'off', baselabel = 'enc_opt_coin_on', baseval = 'on', default = 'on' })
-
-	OptionsManager.registerOption2('ENCUMBRANCE_COLORS', false, 'option_header_game', 'opt_lab_enc_color', 'option_entry_cycler', 
-		{ labels = 'enc_opt_color_off', values = 'off', baselabel = 'enc_opt_color_on', baseval = 'on', default = 'on' })
-
-	OptionsManager.registerOption2('CALCULATE_INVENTORY_VALUE', false, 'option_header_game', 'opt_lab_inv_value', 'option_entry_cycler', 
-		{ labels = 'enc_opt_inv_value_off', values = 'off', baselabel = 'enc_opt_inv_value_on', baseval = 'on', default = 'on' })
-
 	OptionsManager.registerOption2('ENCUMBRANCE_UNIT', false, 'option_header_game', 'opt_lab_enc_unit', 'option_entry_cycler', 
 		{ labels = 'enc_opt_enc_unit_kg|enc_opt_enc_unit_kg-full', values = 'kg|kg-full', baselabel = 'enc_opt_enc_unit_lb', baseval = 'lb', default = 'lb' })
 
 	OptionsManager.registerOption2('WARN_COST', false, 'option_header_game', 'opt_lab_warn_cost', 'option_entry_cycler', 
 		{ labels = 'enc_opt_warn_cost_off|enc_opt_warn_cost_on', values = 'off|on', baselabel = 'enc_opt_warn_cost_subtle', baseval = 'subtle', default = 'subtle' })
-
---	OptionsManager.registerOption2('CARRY_CAPACITY_FROM_EFFECTS', false, 'option_header_game', 'opt_lab_carry_effects', 'option_entry_cycler', 
---		{ labels = 'enc_opt_carry_effects_off', values = 'off', baselabel = 'enc_opt_carry_effects_on', baseval = 'on', default = 'on' })
-
---	OptionsManager.registerOption2('SPEED_INCREMENT', false, 'option_header_game', 'opt_lab_speed_inc', 'option_entry_cycler', 
---		{ labels = 'enc_opt_speed_inc_1', values = '1', baselabel = 'enc_opt_speed_inc_5', baseval = '5', default = '5' })
 end
