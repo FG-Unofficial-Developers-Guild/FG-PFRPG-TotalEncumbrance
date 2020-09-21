79f71ab913520bfe29ae0b88edf48842363e6ba9
Wil Thieme <wil.thieme@protonmail.com>
2020-09-21 19:57:12 -0400

    reimplement coin weight as an inventory item
    doh

--- scripts/coins_weight.lua
+++ scripts/coins_weight.lua
@@ -2,9 +2,9 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
---	This function recomputes the total weight field
-function recomputeTotalWeight(nodeWin)
-	local rActor = ActorManager.getActor('pc', nodeWin)
+--	This function facilitates imperial/metric options
+function recomputeTotalWeight(nodeChar)
+	local rActor = ActorManager.getActor('pc', nodeChar)
 	local nodeChar = DB.findNode(rActor['sCreatureNode'])
 
 	local nEqLoad = DB.getValue(nodeChar, 'encumbrance.load') * TEGlobals.getEncWeightUnit()
@@ -13,8 +13,7 @@ function recomputeTotalWeight(nodeWin)
 		nEqLoad = DB.getValue(nodeChar, 'encumbrance.load')
 	end
 
-	local nTreasure = DB.getValue(nodeChar, 'encumbrance.treasure')
-	local nTotal = nTreasure + nEqLoad
+	local nTotal = nEqLoad
 	local nTotalToSet =	nTotal + 0.5 - (nTotal + 0.5) % 1
 
 	DB.setValue(nodeChar, 'encumbrance.total', 'number', nTotalToSet)
@@ -27,8 +26,6 @@ local function computeCoins(nodeChar)
 	local nWealth = 0
 
 	for _,coin in pairs(DB.getChildren(nodeChar, 'coins')) do
-		nTotalCoins = nTotalCoins + DB.getValue(coin, 'amount', 0)
-		
 		local sDenomination = string.lower(DB.getValue(coin, 'name', ''))
 		local nCoinAmount = DB.getValue(coin, 'amount', 0) + DB.getValue(coin, 'amountA', 0)
 		if sDenomination ~= '' then
@@ -38,12 +35,28 @@ local function computeCoins(nodeChar)
 				end
 			end
 		end
+		nTotalCoins = nTotalCoins + nCoinAmount
 	end
 
-	nTotalCoinWeight = (nTotalCoins / TEGlobals.nCoinsPerUnit) * TEGlobals.getEncWeightUnit()
-	local nTotalCoinWeightToSet =	nTotalCoinWeight + 0.5 - (nTotalCoinWeight + 0.5) % 1
+	local nTotalCoinWeightToSet = math.floor((nTotalCoins / TEGlobals.nCoinsPerUnit) * TEGlobals.getEncWeightUnit())
+	if nTotalCoinWeightToSet then
+		local nodeOtherCoins
+		for _,nodeItem in pairs(DB.getChildren(nodeChar, 'inventorylist')) do
+			local sItemName = string.lower(DB.getValue(nodeItem, 'name', ''))
+			if sItemName == 'coins' then
+				nodeOtherCoins = nodeItem
+			end
+		end
+		if nTotalCoinWeightToSet > 0 and not nodeOtherCoins then nodeOtherCoins = DB.createChild(nodeChar.getChild('inventorylist')) end
+		if nTotalCoinWeightToSet == 0 and nodeOtherCoins then
+			nodeOtherCoins.delete()
+		elseif nodeOtherCoins then
+			DB.setValue(nodeOtherCoins, 'name', 'string', 'Coins')
+			DB.setValue(nodeOtherCoins, 'weight', 'number', nTotalCoinWeightToSet)
+			DB.setValue(nodeOtherCoins, 'description', 'formattedtext', '<p>The standard coin weighs about a third of an ounce (50 to the pound).</p>')
+		end
+	end
 
-	DB.setValue(nodeChar, 'encumbrance.treasure', 'number', nTotalCoinWeightToSet)
 	DB.setValue(nodeChar, 'coins.coinstotalval', 'number', nWealth)
 	
 	recomputeTotalWeight(nodeChar)
